824bfbdcd3648af85495e1b4f3c45f05469c519c
Richard Ayikwei <richardayikweibee@gmail.com>
2025-05-25 16:17:43 +0000

    fix(dep):depency issues

---
+++ src/app/lib/data.ts
@@ -0,0 +1,22 @@
+
+
+export const newfeed = [
+    {
+        title: 'Browse',
+        image: "/Compass_white.png",
+        dark_image: 'Compass_dark.png',
+        dim: 24
+    },
+    {
+        title: 'Watchlist',
+        image: "/Heart_01_dark.png",
+        dark_image: 'Heart_01_white.png',
+        dim: 24
+    },
+    {
+        title: 'Remind',
+        image: "/Bell_dark.png",
+        dark_image: '/Bell_white.png',
+        dim: 24
+    },
+]
---
+++ src/app/ui/side-panel/SidePanel.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import { inter } from "../fonts";
+import { newfeed } from "@/app/lib/data";
+import genres from "@/app/lib/placeholder-data-genre.json";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+function SidePanel() {
+  return (
+    <aside className="w-[14.3125rem] flex-col  bg-[var(--side-panel)]">
+      <h2 className="ml-8 mt-7 text-xl">
+        DXX.<span className="text-[#FF0000]">MOVIES</span>
+      </h2>
+      <section className="w-[7.3125rem] h-[9.4375rem] mt-[7.375rem]">
+        <p className="ml-8">New feed</p>
+        <nav
+          className={`ml-7 text-xs ${inter.variable} antialiased mt-[1.125rem] flex flex-col h-[7.1875rem] justify-between `}
+        >
+          {newfeed.map((feed) => {
+            return (
+              <a
+                href=""
+                className="flex items-center dark:hover:text-slate-400 hover:text-white"
+                key={feed.title}
+              >
+                <Image
+                  src={feed.image}
+                  width={feed.dim}
+                  height={feed.dim}
+                  alt={feed.title}
+                />
+                <span className="ml-[0.8125rem]">{feed.title}</span>
+              </a>
+            );
+          })}
+        </nav>
+      </section>
+      <section className="h-[19.5rem] w-[8.625rem] mt-9 ml-[1.625rem]">
+        <p>Categories</p>
+        <nav className="flex flex-col mt-[1.9375rem] h-[14.4375rem] justify-between">
+          {genres.genres.slice(0, 10).map((genre) => {
+            return (
+              <a
+                href=""
+                key={genre.id}
+                className="dark:hover:text-slate-400 hover:text-white"
+              >
+                {genre.name}
+              </a>
+            );
+          })}
+        </nav>
+        <button
+          type="button"
+          className="mt-[1.4375rem] dark:hover:text-slate-400 hover:text-white flex items-center text-xs "
+        >
+          View more
+          <span>
+            <ChevronRightIcon className="h-[0.75rem] w-[1.333125rem]" />
+          </span>
+        </button>
+      </section>
+    </aside>
+  );
+}
+
+export default SidePanel;
