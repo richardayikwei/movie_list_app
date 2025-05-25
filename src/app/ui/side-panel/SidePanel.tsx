f6b6da591085375689092adc3b9741d50a389475
Richard Ayikwei <richardayikweibee@gmail.com>
2025-05-25 15:47:11 +0000

    ref(side-panel):completed the building

--- src/app/ui/side-panel/SidePanel.tsx
+++ src/app/ui/side-panel/SidePanel.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { inter } from "../fonts";
 import { newfeed } from "@/app/lib/data";
+import genres from '@/app/lib/placeholder-data-genre.json'
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+
+
+
+
 
 function SidePanel() {
   return (
-    <aside className="w-[14.3125rem] flex-col  bg-[#151515]">
+    <aside className="w-[14.3125rem] flex-col  bg-[var(--side-panel)]">
       <h2 className="ml-8 mt-7 text-xl">
         DXX.<span className="text-[#FF0000]">MOVIES</span>
       </h2>
@@ -15,7 +22,11 @@ function SidePanel() {
         >
           {newfeed.map((feed) => {
             return (
-              <a href="" className="flex items-center" key={feed.title}>
+              <a
+                href=""
+                className="flex items-center dark:hover:text-slate-400 hover:text-white"
+                key={feed.title}
+              >
                 <Image
                   src={feed.image}
                   width={feed.dim}
@@ -28,9 +39,27 @@ function SidePanel() {
           })}
         </nav>
       </section>
-      <section className="h-[19.5rem] w-[8.625rem] mt-9 ml-[1.625rem] ">
+      <section className="h-[19.5rem] w-[8.625rem] mt-9 ml-[1.625rem]">
         <p>Categories</p>
-        <nav></nav>
+        <nav className="flex flex-col mt-[1.9375rem] h-[14.4375rem] justify-between">
+          {genres.genres.slice(0, 10).map((genre) => {
+            return (
+              <a
+                href=""
+                key={genre.id}
+                className="dark:hover:text-slate-400 hover:text-white"
+              >
+                {genre.name}
+              </a>
+            );
+          })}
+        </nav>
+        <button
+          type="button"
+          className="mt-[1.4375rem] dark:hover:text-slate-400 hover:text-white flex items-center text-xs "
+        >
+          View more<span><ChevronRightIcon className="h-[0.75rem] w-[1.333125rem]" /></span>
+        </button>
       </section>
     </aside>
   );
